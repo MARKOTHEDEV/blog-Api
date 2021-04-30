09ff4d03654e5ff6cc1a0a369a3cdf02b784fc75
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-04-30 10:47:12 +0800

    finished creating out blog models

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+
+class Blog(models.Model):
+
+    postType = models.CharField(max_length=300)
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    blogPost = models.TextField()
+
+    def __str__(self):
+        return f'{self.author} ----> {self.title}'
+
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return f'{self.user} commented on {self.blog}'
---
+++ users/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+
+# Create your models here.
+
+
+
+class MyuserManager(BaseUserManager):
+    'this class handles the management of the Myuser class'
+    def create_user(self,name,email,password=None):
+        'this function helps create a userInstance using Myuser Model'
+        email = self.normalize_email(email)
+        user = self.model(name=name,email=email)
+        user.set_password(password)
+        
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,name,password=None):
+        'when we call the create_user it returns a newly created instance all we need to do is to attach so permiisions to it'
+        user = self.create_user(email=email,name=name,password=password)
+        user.is_staff = True
+        user.is_superuser =True
+        user.save(using=self._db)
+
+        return user
+
+
+class Myuser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=300)
+    # image = models.ImageField(upload_to='userImage/%d/',default='user.jpg')
+    bio = models.TextField()
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    # so instead of using name to login we use email and password
+    USERNAME_FIELD = 'email'
+
+
+
+    # this tells django how to mamange our Myuser model
+    objects = MyuserManager()
+
+    def __str__(self):
+        return self.name or self.email
+
+
+
