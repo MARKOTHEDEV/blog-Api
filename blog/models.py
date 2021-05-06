79c2b612c05c8c85936ce8b130281b3b89f967a0
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-05-06 08:44:19 +0800

    Modified the database now we can add more postType e.g entertainment,programming in the future in this way it more scaleable

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+class PostType(models.Model):
+    'this model let the blog website so in feature we can add more blog post'
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
 class Blog(models.Model):
 
-    postType = models.CharField(max_length=300)
+    postType = models.ForeignKey(PostType,on_delete=models.SET_NULL,null=True)
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
     blogPost = models.TextField()
