1375f680a2aa5e698d69e23a9e902db58fdd5b86
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-05-15 06:30:55 +0800

    succesfully fixed the detail page hero section, now u can click any link and go to a detail page and it will load the content dynamically

--- frontend/static/apijs/blog-details.js
+++ frontend/static/apijs/blog-details.js
@@ -1,14 +1,28 @@
 // blogId is comming from the js script that is directly placed to this one 
 // it is a const so u can't change it
 
+class UI{
+    constructor(){
+        this.title = document.querySelector('#title');
+        this.blogDetailhero_image = document.querySelector('#blogDetailhero_image');
+        this.hero_section_categorie = document.querySelector('#hero_section_categorie');
+        this.hero_section_authorName = document.querySelector('#hero_section_authorName')
+        this.hero_section_Datecreated = document.querySelector('#hero_section_Datecreated');
+        this.hero_section_authorImage = document.querySelector('#hero_section_authorImage')
+        // console.log(this.hero_section_Datecreated)
+        
+    }
 
 
+    
+}
 
 class BlogDetails{
 
 
     constructor(BlogDetailUrl){
-        this.url = BlogDetailUrl
+        this.url = BlogDetailUrl;
+        this.ui = new UI()
     }
 
     //this get the blog detail
@@ -22,9 +36,15 @@ class BlogDetails{
     }
 
 
+
     displayBlogDetail(data){
 
-        
+        // first set the h1 or the title of the page
+        // this code handles all the contenton the  hero section
+        this.ui.title.innerText =data.title
+        this.ui.blogDetailhero_image.style.backgroundImage = `url(${data.contentHeader})`
+        this.ui.hero_section_authorImage.src = data.authorImage
+        this.ui.hero_section_Datecreated.innerText =data.dateCreated
     }
 
 
@@ -32,6 +52,7 @@ class BlogDetails{
         this.getBlogDetails(this.url)
         .then(data =>{
             console.log('data',data)
+            this.displayBlogDetail(data)
         })
         .catch(err=>{
             console.log(err)
