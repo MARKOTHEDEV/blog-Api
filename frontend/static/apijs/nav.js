79ca32a3190b29416428141c15c97ffafec84b39
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-05-17 09:34:20 +0800

    create to nav one for auth and the other of the unauth user for better user expirence

---
+++ frontend/static/apijs/nav.js
@@ -0,0 +1,19 @@
+
+
+let user = JSON.parse(sessionStorage.getItem('user'))
+let userToken = JSON.parse(sessionStorage.getItem('userToken'))
+let navlinksForAuthUsers = document.querySelector('#navlinksForAuthUsers')
+let navlinksForUnAuthUsers = document.querySelector('#navlinksForUnAuthUsers')
+let userNavLinkName = document.querySelector('#userNavLinkName')
+ 
+console.log(navlinksForAuthUsers)
+if(user === null && userToken === null){
+    //if the person is not logged in 
+    // navlinksForAuthUsers.style.display='none';
+    navlinksForAuthUsers.remove()
+}
+else{
+    //if the person is  logged in 
+    userNavLinkName.innerText =`Hello ${user.email}`
+    navlinksForUnAuthUsers.remove()
+}
